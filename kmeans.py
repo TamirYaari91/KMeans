2721537167230e70c610c8368360ff7794b3213a
TamirYaariTAU <73110585+TamirYaariTAU@users.noreply.github.com>
2021-04-27 23:52:50 +0300

    Update kmeans.py

--- kmeans.py
+++ kmeans.py
@@ -1,10 +1,10 @@
 import sys
+
 lines = sys.stdin.readlines()
 
 points = [0 for i in range(len(lines))]
 for i in range(len(lines)):
-    points[i] = lines[i].split(',')
-    points[i] = [float(coord) for coord in points[i]]
+    points[i] = [float(coord) for coord in lines[i].split(',')]
 
 k = int(sys.argv[1])
 max_iter = 200
@@ -14,6 +14,7 @@ centroids = [0.0] * k
 point_to_cluster = dict()
 dim = len(points[0])
 
+
 def initialize():
     for i in range(k):
         centroids[i] = points[i]
@@ -21,7 +22,7 @@ def initialize():
         point_to_cluster[tuple(points[i])] = -1
 
 
-def distance(p,cluster):
+def distance(p, cluster):
     d = 0
     for i in range(dim):
         d += (p[i] - centroids[cluster][i]) * (p[i] - centroids[cluster][i])
@@ -31,12 +32,13 @@ def distance(p,cluster):
 
 def set_cluster(p):
     min_index = 0
-    distances = [0]*k
+    distances = [0] * k
     for i in range(k):
-        distances[i] = distance(p,i)
+        distances[i] = distance(p, i)
         if distances[i] < distances[min_index]:
             min_index = i
     point_to_cluster[tuple(p)] = min_index
+
 
 def update_centroids():
     cluster_to_points = dict()
@@ -49,21 +51,20 @@ def update_centroids():
 
     changed = False
     for i in range(k):
-        new_centroid = cluster_mean(i,cluster_to_points)
+        new_centroid = cluster_mean(i, cluster_to_points)
         if (centroids[i] != new_centroid):
             centroids[i] = new_centroid
             changed = True
     return changed
 
 
-
-def cluster_mean(cluster,c2p):
-    center = [0.0]*dim
+def cluster_mean(cluster, c2p):
+    center = [0.0] * dim
     val = 0
     for i in range(dim):
         for point in c2p[cluster]:
             val += point[i]
-        center[i] = val/len(c2p[cluster])
+        center[i] = val / len(c2p[cluster])
         val = 0
     return center
 
@@ -87,7 +88,6 @@ k_means_clustering()
 for i in range(k):
     for j in range(dim):
         centroids[i][j] = '{0:.4f}'.format(centroids[i][j])
-
 
 for cent in centroids:
     to_print = ','.join(cent)
